5600f0ce74c3b70394aa0fcf208b05659e03ced4
Robert Santos <robertdosantos09@gmail.com>
2023-03-24 20:16:31 -0300

    feat: Adicionando codigos a função filter_by_job_type, para o requisito 6.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -37,18 +37,13 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    """Filters a list of jobs by job_type"""
+    returned_list_of_job_type = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    for i in jobs:
+        if (i['job_type'] == job_type):
+            returned_list_of_job_type.append(i)
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return returned_list_of_job_type
+
+    # raise NotImplementedError
