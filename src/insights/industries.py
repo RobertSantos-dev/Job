c3ed3c18dc078ece9ae677de349d8aa0c4854607
Robert Santos <robertdosantos09@gmail.com>
2023-03-24 18:13:46 -0300

    feat: Adicionando codigos a função get_unique_industries, para o requisito 3.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,21 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    """Checks all different industries and returns a list of them"""
+    return_from_list_of_industries = read(path)
+    set_of_industry = set()
+    list_converted_from_set = []
 
-    Must call `read`
+    for industry in return_from_list_of_industries:
+        if(industry['industry'] != ''):
+            set_of_industry.add(industry['industry'])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    list_converted_from_set.extend(set_of_industry)
+    return list_converted_from_set
 
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    # raise NotImplementedError
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
