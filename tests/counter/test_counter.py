1cf73a16fb727365c055923671339bf1ee3c7d22
Robert Santos <robertdosantos09@gmail.com>
2023-03-25 04:21:29 -0300

    tests: Adicionando testes para a função count_ocurrences.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', 'INTERNAL') == 1171
+
+    with pytest.raises(AttributeError):
+        count_ocurrences('data/jobs.csv', -1)
+
+    with pytest.raises(AttributeError):
+        count_ocurrences('data/jobs.csv', None)
+
+    with pytest.raises(AttributeError):
+        count_ocurrences('data/jobs.csv', [])
+
+    with pytest.raises(AttributeError):
+        count_ocurrences('data/jobs.csv', {})
