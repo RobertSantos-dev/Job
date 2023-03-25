33b3921c8950503b5e81e364a0346b75f4ed2cf5
Robert Santos <robertdosantos09@gmail.com>
2023-03-24 21:06:14 -0300

    feat: Adicionando codigos a função filter_by_industry, para o requisito 7.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 from src.insights.jobs import read
+# from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -19,18 +20,13 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    """Filters a list of jobs by industry"""
+    returned_list_of_industry = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for i in jobs:
+        if (i['industry'] == industry):
+            returned_list_of_industry.append(i)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return returned_list_of_industry
+
+    # raise NotImplementedError
